9f1b3cf42ca16951925dbad7738844e4ab8ce135
Mike Wood <wood.michaelthomas@gmail.com>
2019-12-04 11:13:45 -0800

    Added cleaning features for data_frame

--- create_ppt.py
+++ create_ppt.py
@@ -8,6 +8,7 @@ from pptx import Presentation
 # from pptx.util import Inches
 import argparse
 import pandas as pd
+import numpy as np
 import os
 from datetime import date
 
@@ -17,67 +18,66 @@ from datetime import date
 TITLE_SLIDE_LAYOUT = 0
 TITLE_SLIDE = {'subtitle': 1}
 
+# Placeholder layout for blank slides
 BLANK_SLIDE_LAYOUT = 7
 
 # Placeholder indices for the first approval slide
-APP_SLIDE1_LAYOUT = 2
-APP_SLIDE1 = {'Key': 27,
-              'Sponsor Department': 23,
-              'Description': 1,
-              'Status': 24,
-              'Return Type': 25,
-              'ROI': 15,
-              'Return': 16,
-              'Total Investment': 17,
-              'Sponsor': 18,
-              'Project Owner': 19,
-              'Line of Business': 21,
-              'Return Description': 28,
-              'Investment Description': 13,
-              'Project Manager': 20,
-              'States': 22,
-              }
+PAP_SLIDE1_LAYOUT = 2
+PAP_SLIDE1_COLUMN_MAPPING = {'Key': 27,
+                             'Sponsor Department': 23,
+                             'Description': 1,
+                             'Status': 24,
+                             'Return Type': 25,
+                             'ROI': 15,
+                             'Return': 16,
+                             'Total Investment': 17,
+                             'Sponsor': 18,
+                             'Line of Business': 21,
+                             'Return Description': 28,
+                             'Investment Description': 13,
+                             'Project Manager': 20,
+                             'States': 22,
+                             }
 
 # Placeholder indices for the second approval slide
-APP_SLIDE2_LAYOUT = 3
-APP_SLIDE2 = {'Key': 27,
-              'Scope': 14,
-              'Scope Exclusions': 15,
-              'Systems': 17,
-              'Business Area Impacts': 16,
-              }
+PAP_SLIDE2_LAYOUT = 3
+PAP_SLIDE2_COLUMN_MAPPING = {'Key': 27,
+                             'Scope': 14,
+                             'Scope Exclusions': 15,
+                             'Systems': 17,
+                             'Business Area Impacts': 16,
+                             }
 
 # Data Column Names for simplified mapping
-COLMUN_NAMES = {'Issue key': 'Key',
-                'Issue id': 'ID',
-                'Created': 'Created',
-                'Summary': 'Summary',
-                'Custom field (Sponsor Department)': 'Sponsor Department',
-                'Custom field (Project Classification)':
-                    'Classification',
-                'Description': 'Description',
-                'Status': 'Status',
-                'Custom field (Return Type)': 'Return Type',
-                'Custom field (Return on Investment)': 'ROI',
-                'Custom field (Break Even Period)': 'Break Even Period',
-                'Custom field (Return)': 'Return',
-                'Custom field (Duration)': 'Duration',
-                'Custom field (Total Investment)': 'Total Investment',
-                'Custom field (Labor T-Shirt Size)': 'Labor T-Shirt Size',
-                'Custom field (Labor Investment)': 'Labor Investment',
-                'Custom field (Non-Labor Investment)': 'Non-Labor Investment',
-                'Custom field (Sponsor)': 'Sponsor',
-                'Custom field (Business Area Impacts)':
-                    'Business Area Impacts',
-                'Custom field (Labor Investment Description)':
-                    'Investment Description',
-                'Custom field (Line of Business)': 'Line of Business',
-                'Custom field (Return Description)': 'Return Description',
-                'Custom field (Scope)': 'Scope',
-                'Custom field (Scope Exclusions)': 'Scope Exclusions',
-                'Custom field (Systems)': 'Systems',
-                'Custom field (Project Manager)': 'Project Manager',
-                }
+COLUMNS = {'Issue key': 'Key',
+           'Issue id': 'ID',
+           'Created': 'Created',
+           'Summary': 'Summary',
+           'Custom field (Sponsor Department)': 'Sponsor Department',
+           'Custom field (Project Classification)':
+               'Classification',
+           'Description': 'Description',
+           'Status': 'Status',
+           'Custom field (Return Type)': 'Return Type',
+           'Custom field (Return on Investment)': 'ROI',
+           'Custom field (Break Even Period)': 'Break Even Period',
+           'Custom field (Return)': 'Return',
+           'Custom field (Duration)': 'Duration',
+           'Custom field (Total Investment)': 'Total Investment',
+           'Custom field (Labor T-Shirt Size)': 'Labor T-Shirt Size',
+           'Custom field (Labor Investment)': 'Labor Investment',
+           'Custom field (Non-Labor Investment)': 'Non-Labor Investment',
+           'Custom field (Sponsor)': 'Sponsor',
+           'Custom field (Business Area Impacts)': 'Business Area Impacts',
+           'Custom field (Labor Investment Description)':
+               'Investment Description',
+           'Custom field (Line of Business)': 'Line of Business',
+           'Custom field (Return Description)': 'Return Description',
+           'Custom field (Scope)': 'Scope',
+           'Custom field (Scope Exclusions)': 'Scope Exclusions',
+           'Custom field (Systems)': 'Systems',
+           'Custom field (Project Manager)': 'Project Manager',
+           }
 
 STATUS_MAPPING = {"Project Requests": ["New Request",
                                        "Initial Review",
@@ -93,6 +93,8 @@ STATUS_MAPPING = {"Project Requests": ["New Request",
                   "Complete Projects": ["Project Complete"],
                   "Rejected Projects": ["Project Rejected"],
                   }
+
+COLUMNS_WITH_NAMES = ['Project Manager', 'Sponsor']
 
 
 def createSlide(prs, layout_index):
@@ -156,20 +158,31 @@ def placeTextInSlide(slide, placeholderIndex, text):
 def convert_usernames_to_fullnames(data_frame):
     ''' compares a set of columnns against known usernames and converts them
         to full names '''
+    # get the dictionary from the name_mapping.csv
     name_df = pd.read_csv("name_mapping.csv")
-    name_dict = name_df.to_dict()
     name_dict = {name_df['Username'][ind]: name_df['Full Name'][ind]
                  for ind in name_df.index}
+    for column in COLUMNS_WITH_NAMES:
+        data_frame[column] = data_frame[column].map(name_dict)
+    return data_frame
 
-    data_frame["Project Manager"] = data_frame["Project Manager"].map(
-                                        name_dict)
-    data_frame["Sponsor"] = data_frame["Sponsor"].map(
-                                        name_dict)
+
+def reduce_data_frame_columns(data_frame):
+    ''' reduces the columns to just those needed for the pap'''
+    # get the needed column names for the slide decks
+    needed_columns = (PAP_SLIDE1_COLUMN_MAPPING.keys() +
+                      PAP_SLIDE2_COLUMN_MAPPING.keys())
+    # filter the data_frame to just those needed columnns
+    data_frame = data_frame.filter(needed_columns.unique())
     return data_frame
 
 
 def clean_data_frame(data_frame):
     ''' Cleans the data in the data_frame. '''
+    # Update column names in the data_frame
+    data_frame.rename(columns=COLUMNS, inplace=True)
+    # reduce the columns to those needed
+    data_frame = reduce_data_frame_columns(data_frame)
     # change usernames to Full Names
     data_frame = convert_usernames_to_fullnames(data_frame)
     return data_frame
@@ -177,19 +190,18 @@ def clean_data_frame(data_frame):
 
 def getDataFrame(data_file):
     data_frame = pd.read_csv(data_file)
-    data_frame.rename(columns=COLMUN_NAMES, inplace=True)
     data_frame = clean_data_frame(data_frame)
     return data_frame
 
 
-def create_pap_pptx(df, prs, dept, output_filename):
+def create_pap_pptx(df, prs, item, output_filename):
     # Insert title slide with department and status_category such as
     #    project request, inflight or complete.
     for status_category in STATUS_MAPPING:
         statuses = STATUS_MAPPING[status_category]
         status_dataframe_subset = df.loc[df['Status'].isin(statuses)]
         if not status_dataframe_subset.empty:
-            title_txt = dept + ' ' + status_category
+            title_txt = item + ' ' + status_category
             subtitle_txt = ('\nCreated on {:%m-%d-%Y}'.format(date.today())
                             )
             placeholder_list = [(TITLE_SLIDE['subtitle'], subtitle_txt)]
@@ -201,26 +213,30 @@ def create_pap_pptx(df, prs, dept, output_filename):
             # create the slides in status order
             for status in STATUS_MAPPING[status_category]:
                 # subset the dataframe to the department rows
-                print(dept, status)
-                df_subset = df.loc[(df['Sponsor Department'] == dept) &
-                                   (df['Status'] == status)]
+                df_subset = df.loc[(df['Status'] == status)]
                 df_subset = df_subset.sort_values(by=['Summary'])
-                # Proposed Project Slides
+                i = 0
                 for index, series in df_subset.iterrows():
-                    print('Creating approval slide', index + 1, 'of',
+                    if (len(df_subset.index) > 0) and (i == 0):
+                        print(item, '-', status)
+                        # Proposed Project Slides
+                    i += 1
+                    print('Creating slide', i, 'of',
                           len(df_subset.index), '.')
                     title_txt = series["Summary"]
-                    populateSlideFromSeries(createSlide(prs,
-                                                        APP_SLIDE1_LAYOUT),
-                                            title_txt, APP_SLIDE1, series)
-                    populateSlideFromSeries(createSlide(prs,
-                                                        APP_SLIDE2_LAYOUT),
-                                            title_txt, APP_SLIDE2, series)
+
+                    first_pap_slide = createSlide(prs, PAP_SLIDE1_LAYOUT)
+                    populateSlideFromSeries(first_pap_slide, title_txt,
+                                            PAP_SLIDE1_COLUMN_MAPPING, series)
+
+                    second_pap_slide = createSlide(prs, PAP_SLIDE2_LAYOUT)
+                    populateSlideFromSeries(second_pap_slide, title_txt,
+                                            PAP_SLIDE2_COLUMN_MAPPING, series)
 
         prs.save(output_filename)
 
 
-def create_pap_pptxs(dataframe, ppt_template, file_prefix):
+def create_pap_pptxs(data_frame, ppt_template, file_prefix):
     '''Create a project approval ppt from a dataframe and ppt template.
         A pap ppt deck will be created for each dept with the output prefix.'''
 
@@ -232,23 +248,30 @@ def create_pap_pptxs(dataframe, ppt_template, file_prefix):
     # create a base path and filename for the ppts
     ppt_base_file_path = os.path.join(
                         ppt_folder_path,
-                        os.path.basename(file_prefix) + '.pptx')
+                        os.path.basename(file_prefix))
+
+    # slice determines the breakdown of project requests into ppts.
+    slice = 'Sponsor'
+
+    # any slice that is blank or nan should be marked unknown
+    data_frame[slice] = data_frame[slice].replace(np.nan, 'Unknown',
+                                                  regex=True)
 
     # create a ppt deck for each department
-    for dept in dataframe['Sponsor Department'].unique():
+    for item in data_frame[slice].unique():
         # build a ppt deck for each of the departments
         ppt_deck = Presentation(ppt_template)
 
         # create the output filename using the file_prefix and department
         output_ppt_filename = ppt_base_file_path.replace(
-                                '.pptx', '_' + str(dept).replace(' ', '_') +
+                                '.pptx', '_' + str(item).replace(' ', '_') +
                                 '.pptx')
         # get the department specific data
-        dept_dataframe = dataframe.loc[dataframe['Sponsor Department'] == dept]
+        item_dataframe = data_frame.loc[data_frame[slice] == item]
 
         # create the powerpoint for the department
-        print('Creating', dept, 'ppt.')
-        create_pap_pptx(dept_dataframe, ppt_deck, dept, output_ppt_filename)
+        print('Creating', item, 'ppt.', len(item_dataframe))
+        create_pap_pptx(item_dataframe, ppt_deck, item, output_ppt_filename)
 
 
 if __name__ == "__main__":
